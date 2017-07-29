84728c0409df59ecc09fd2d1431a1745a4f47e41
Robert Koch <robert@kochie.io>
2017-07-30 00:22:33 +1000

    Added information to README

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # TAHEAP-ML
 Traffic Accident Heatmap Prediction ML Code
+
+
+# Install
+
+It is recommended that the network is run in a virtual environment. To create one using virtualenvwrapper run:
+
+```bash
+mkvirtualenv TensorFlow
+workon TensorFlow
+```
+
+Now install requirements and run.
+
+```bash
+pip install -r requirements.txt
+
+./run.sh
+```
--- src/main.py
+++ src/main.py
@@ -3,31 +3,20 @@ import tensorflow as tf
 
 mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 
-sess = tf.InteractiveSession()
-
 x = tf.placeholder(tf.float32, shape=[None, 784])
-y_ = tf.placeholder(tf.float32, shape=[None, 10])
-
-W = tf.Variable(tf.zeros([784,10]))
-b = tf.Variable(tf.zeros([10]))
-
-sess.run(tf.global_variables_initializer())
-y = tf.matmul(x,W) + b
-
-cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y))
-
-train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
-
-correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
-
-accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-print(accuracy.eval(feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
+y_ = tf.placeholder(tf.float32, shape=[None, 2])
 
 def weight_variable(shape):
+    """
+    Outputs a random tf variable from a truncated normal distribution. 
+    """
     initial = tf.truncated_normal(shape, stddev=0.1)
     return tf.Variable(initial)
 
 def bias_variable(shape):
+    """
+    Creates constant tensor variables.
+    """
     initial = tf.constant(0.1, shape=shape)
     return tf.Variable(initial)
 
@@ -37,6 +26,8 @@ def conv2d(x, W):
 def max_pool_2x2(x):
     return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
 
+
+# Weights and biases for layer 1
 W_conv1 = weight_variable([5, 5, 1, 32])
 b_conv1 = bias_variable([32])
 
@@ -45,12 +36,14 @@ x_image = tf.reshape(x, [-1, 28, 28, 1])
 h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
 h_pool1 = max_pool_2x2(h_conv1)
 
+# Weights and biases for layer 2
 W_conv2 = weight_variable([5, 5, 32, 64])
 b_conv2 = bias_variable([64])
 
 h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
 h_pool2 = max_pool_2x2(h_conv2)
 
+# Weights and biases for fully connected layer.
 W_fc1 = weight_variable([7 * 7 * 64, 1024])
 b_fc1 = bias_variable([1024])
 
@@ -60,13 +53,12 @@ h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
 keep_prob = tf.placeholder(tf.float32)
 h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
 
-W_fc2 = weight_variable([1024, 10])
-b_fc2 = bias_variable([10])
+W_fc2 = weight_variable([1024, 2])
+b_fc2 = bias_variable([2])
 
 y_conv = tf.matmul(h_fc1_drop, W_fc2) + b_fc2
 
-cross_entropy = tf.reduce_mean(
-    tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y_conv))
+cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y_conv))
 train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
 correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_, 1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
